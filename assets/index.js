651b20760a01ee6c92079124607ff79ea55b3798
Alyona <agerneta@gmail.com>
2021-02-03 16:52:35 +0200

    Add promise Cat creation

--- assets/index.js
+++ assets/index.js
@@ -28,3 +28,26 @@ const p = fetch('./assets/data.json')
   .then((response) => response.json())
   .then((result) => console.log(result))
   .catch((err) => console.error(err));
+
+
+
+  
+const promiseShchrodinger = new Promise(executor);
+
+function executor(resolve, reject) {
+  const catsFate = Math.random();
+
+  if (catsFate >= 0.5) {
+    resolve('cat is alive');
+  } else {
+    reject('dead cat');
+  }
+}
+
+promiseShchrodinger
+  .then((catVerdict) => {
+    console.log(catVerdict);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
